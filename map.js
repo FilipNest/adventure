c60090cd32044670a373745f80097aa7106fb477
Filip Hnízdo <filip@bluejumpers.com>
2014-04-23 17:44:42 +0100

    Create new branch
    Make playable without graphical user interface

--- map.js
+++
@@ -1,9 +0,0 @@
-var map = L.mapbox.map('map', 'filipnest.ga46fcfi')
-    .setView([51.53333600874287, -0.1788496971130371 ], 17);
-
-map.on('click', function(e) {
-var lat = e.latlng.lat;
-var lng = e.latlng.lng;
-var marker = L.marker([lat, lng]).addTo(map);
-newpoint();
-});
