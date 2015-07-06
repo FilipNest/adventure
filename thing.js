0453006f065a3d5e2dbea6fb5b0410a5cc4a8859
Filip Hnízdo <filip@bluejumpers.com>
2015-07-06 23:08:45 +0100

    Public functions for things and holder of them

--- thing.js
+++ thing.js
@@ -1,75 +1,114 @@
 var A = {};
+
+//Holder for all things
+
 A.things = {};
 
-A.thing = {
+A.thing = function (name, type, hidden, value) {
 
-  name: null,
-  type: null, //String or number
-  hidden: true,
-  value: null,
-  create: function (name, type, hidden, value) {
+  //Create object
 
-    if (typeof name !== "string") {
+  if (typeof name !== "string") {
 
-      throw Error("Not a string");
+    throw Error("Not a string");
 
-    }
-
-    if (type !== "string" && type !== "number") {
-
-      throw Error("Type must be either string or number");
-
-    }
-
-    if (type === "string" && typeof value !== "string" || type === "number" && typeof value !== "number") {
-
-      throw Error("Not a" + +value);
-
-    }
-
-    if (type === "string" && typeof value !== "string" || type === "number" && typeof value !== "number") {
-
-      throw Error("Not a" + +value);
-
-    }
-
-    if (typeof hidden !== "boolean") {
-
-      throw Error("Hidden value must be true or false");
-
-    }
-
-    //Check if thing doesn't already exist in the world
-
-    if (A.things[name]) {
-
-      throw Error("There is already a thing called " + name);
-
-    }
-
-    //Create thing
-
-    var thing = Object.create(A.thing, {
-
-      name: {
-        writable: false,
-        value: name
-      },
-      type: {
-        writable: false,
-        value: type
-      },
-      hidden: {
-        writable: true,
-        value: hidden
-      },
-      value: {
-        writable: true,
-        value: value
-      }
-    });
-
-    return A.things[name] = thing;
-    
   }
+
+  if (type !== "string" && type !== "number") {
+
+    throw Error("Type must be either string or number");
+
+  }
+
+  if (type === "string" && typeof value !== "string" || type === "number" && typeof value !== "number") {
+
+    throw Error("Not a" + +value);
+
+  }
+
+  if (type === "string" && typeof value !== "string" || type === "number" && typeof value !== "number") {
+
+    throw Error("Not a" + +value);
+
+  }
+
+  if (typeof hidden !== "boolean") {
+
+    throw Error("Hidden value must be true or false");
+
+  }
+
+  //Check if thing doesn't already exist in the world
+
+  if (A.things[name]) {
+
+    throw Error("There is already a thing called " + name);
+
+  }
+
+  //Set values if validation passes
+
+  this.name = name;
+  this.type = type;
+  this.hidden = hidden;
+  this.value = value;
+
+  var self = this;
+
+  var public = {
+
+    setValue: function (value) {
+
+      if (self.type === typeof value) {
+
+        self.value = value;
+
+      } else {
+
+        throw Error("Needs to be " + type);
+
+      }
+
+    },
+
+    getValue: function () {
+
+      return self.value;
+
+    },
+
+    setHidden: function (hidden) {
+
+      if (typeof hidden === "boolean") {
+
+        self.hidden = hidden;
+
+      } else {
+
+        throw Error("Hidden must be true or false");
+
+      }
+
+    },
+
+    flipHidden: function () {
+
+      self.hidden = !self.hidden;
+      
+      return self.hidden;
+
+    },
+
+    checkHidden: function () {
+
+      return self.hidden;
+
+    }
+
+  }
+  
+  A.things[name] = public;
+  
+  return public;
+
 };
