3409aaccf15c8f3c3bdd50e68831cb31b91912aa
Filip Hnízdo <filip@bluejumpers.com>
2015-07-12 23:11:23 +0100

    Function for listing visible things

--- all/modules/custom/adventure_core/js/front.js
+++ all/modules/custom/adventure_core/js/front.js
@@ -3,7 +3,29 @@
    $.each(A.data, function(index, element){
 
      new A.thing(element.id, element.name,element.description, element.value, element.viewing_requirements);
-  
+       
+   });
+   
+   A.visibleThings = function(){
+     
+     var visible = [];
+     
+     $.each(A.things, function(index, thing){
+       
+       if(thing.checkVisibility()){
+         
+         visible.push(thing);
+         
+       };
+       
+     });
+     
+     return visible;
+     
+   };
+
+ })(jQuery)
+
 //  var action = new A.action("Fun", 50000, "You increased your fun");
 //
 //  var action2 = new A.action("Fun", 30000, "You increased your fun");
@@ -14,7 +36,3 @@
 //  var choice2 = new A.choice("This is a choice", chain, action);
 //
 ////choice.trigger();
-     
-   });
-
- })(jQuery)
